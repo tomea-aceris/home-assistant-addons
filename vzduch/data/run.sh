b1b98656d5e4d337c20c71fe72403c4d507630de
tomea-aceris <115330371+tomea-aceris@users.noreply.github.com>
2025-06-13 06:41:09 +1000

    Update run.sh

--- vzduch/data/run.sh
+++ vzduch/data/run.sh
@@ -1,24 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 set -e
 
-echo "📡 Reading Airtouch config..."
+bashio::log.info "Airtouch configuration..."
 
-# Read config values passed in from config.json using Home Assistant env var file
-CONFIG_PATH="/data/options.json"
+AIRTOUCH_HOST=$(bashio::config 'airtouch_host')
+AIRTOUCH_PORT=$(bashio::config 'airtouch_port') 
+AIRTOUCH_LOGLEVEL=$(bashio::config 'logLevel') 
 
-AIRTOUCH_HOST=$(jq -r '.airtouch_host' "$CONFIG_PATH")
-AIRTOUCH_PORT=$(jq -r '.airtouch_port' "$CONFIG_PATH")
-AIRTOUCH_LOGLEVEL=$(jq -r '.logLevel' "$CONFIG_PATH")
-
-echo "✔️ Host: $AIRTOUCH_HOST"
-echo "✔️ Port: $AIRTOUCH_PORT"
-echo "✔️ Log Level: $AIRTOUCH_LOGLEVEL"
-
-# Export for the .NET app
 export DOTEK_airTouch__localHost=$AIRTOUCH_HOST
 export DOTEK_airTouch__localPort=$AIRTOUCH_PORT
 export DOTEK_Serilog__MinimumLevel__Default=$AIRTOUCH_LOGLEVEL
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
 
-echo "🚀 Starting VzduchDotek.Net..."
 exec ./VzduchDotek.Net
